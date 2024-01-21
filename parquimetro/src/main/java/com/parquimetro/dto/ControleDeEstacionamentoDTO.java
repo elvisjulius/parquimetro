16a2b0228ffb3d06e41c396320e0215a3c3eae99
Elvis <elvisjulius@hotmail.com>
2024-01-21 18:45:54 -0300

    adicionando fluxo de cobrança

---
+++ parquimetro/src/main/java/com/parquimetro/dto/ControleDeEstacionamentoDTO.java
@@ -0,0 +1,35 @@
+package com.parquimetro.dto;
+
+import com.parquimetro.entity.Condutor;
+import com.parquimetro.entity.ControleDeEstacionamento;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public record ControleDeEstacionamentoDTO(
+        UUID id,
+        LocalDateTime horaEntrada,
+        LocalDateTime horaSaida,
+        @NotNull(message = "O tipo de cobrança deve ser 'FIXA' ou 'VARIAVEL")
+        @NotEmpty(message = "O tipo de cobrança deve ser 'FIXA' ou 'VARIAVEL")
+        String tipoCobranca,
+        @NotNull(message = "Veiculos do  Condutor não ser nulo")
+        @NotEmpty(message = "Veiculos do Condutor não ser vazia")
+        VeiculoDTO veiculos
+) {
+    public ControleDeEstacionamento toEntity() {
+        ControleDeEstacionamento controleDeEstacionamento = new ControleDeEstacionamento();
+        controleDeEstacionamento.setId(this.id());
+        controleDeEstacionamento.setHoraEntrada(this.horaEntrada);
+        controleDeEstacionamento.setHoraSaida(this.horaSaida);
+        controleDeEstacionamento.setTipoCobranca(this.tipoCobranca);
+        controleDeEstacionamento.setVeiculoUtilizado(this.veiculos.toEntity());
+        return controleDeEstacionamento;
+    }
+
+}
